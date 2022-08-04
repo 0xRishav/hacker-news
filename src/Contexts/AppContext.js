1041b0553e7f651f9c51986a5c02da8c63542522
Rishav Bharti <rishav.bharti@omnycomm.com>
2022-08-04 08:06:52 +0530

    fix: AppContext value prop

--- src/Contexts/AppContext.js
+++ src/Contexts/AppContext.js
@@ -4,8 +4,9 @@ export const AppContext = createContext();
 
 export const AppContextProvider = ({ children }) => {
   const [searchResults, setSearchResults] = useState([]);
+  const BaseUrl = "http://hn.algolia.com/api/v1/";
   return (
-    <AppContext.Provider value={{ searchResults, setSearchResults }}>
+    <AppContext.Provider value={{ searchResults, setSearchResults, BaseUrl }}>
       {children}
     </AppContext.Provider>
   );
