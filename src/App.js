f0b83a27b1caf07e9aac4e52d57734532a8bd2a1
Rishav Bharti <rishav.bharti@omnycomm.com>
2022-08-04 08:07:35 +0530

    feat: add basic search

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useContext } from "react";
+import { AppContext } from "./Contexts/AppContext";
+import { getSearchApi } from "./Contexts/APIs";
+import axios from "axios";
 
 function App() {
+  const { searchResults, setSearchResults, BaseUrl } = useContext(AppContext);
+
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    try {
+      const searchApi = getSearchApi(e.target.children[0].value);
+      const res = await axios.get(searchApi);
+      setSearchResults(res?.data?.hits);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
     <div className="App">
-      <div>HackerNews</div>
+      <header>
+        <div>HackerNews</div>
+        <form onSubmit={handleSearch}>
+          <input type="text" placeholder="Search here..." />
+        </form>
+      </header>
+      <div>
+        {searchResults.map((hit) => (
+          <>
+            <h1>{hit.title}</h1>
+            <div>{hit.author}</div>
+            <div>{hit.created_at}</div>
+            <div>{hit.points}</div>
+          </>
+        ))}
+      </div>
     </div>
   );
 }
